a5339a6507a50ec07dff1c61756f1cdeaf334891
Erik Nelson <erik@nsk.io>
2023-05-11 21:53:53 -0400

    Initial django commit

---
+++ pistosi_sbx/pistosi_sbx/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pistosi_sbx project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pistosi_sbx.settings')
+
+application = get_asgi_application()
