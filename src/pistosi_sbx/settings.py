8f773a8589075fc5439a9f9df6929bc9e214492a
Erik Nelson <erik@nsk.io>
2023-05-12 14:24:37 -0400

    Read SECRET_KEY from the env

--- src/pistosi_sbx/settings.py
+++ src/pistosi_sbx/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*_u3a0_26wcl!d60846%_d232g0wjl=2r9+go2z)el_8*)d+mm'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
